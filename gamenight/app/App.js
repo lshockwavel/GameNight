b10ec032ebf6f146134113153d796e990e6a4720
ONETEAM\DaneBurge <daneburge@truckstop.com>
2025-04-12 12:31:35 -0600

    minor cleanup, making some progress

--- gamenight/app/App.js
+++ gamenight/app/App.js
@@ -1,8 +1,8 @@
-import { ExampleController } from './controllers/ExampleController.js';
+import { PlayersController } from "./controllers/PlayersController.js"
 
 class App {
 
-  ExampleController = new ExampleController() // ☑️ you can remove this - example only
+  PlayersController = new PlayersController();
 
 }
 
--- gamenight/app/controllers/PlayersController.js
+++ gamenight/app/controllers/PlayersController.js
@@ -5,4 +5,12 @@ export class PlayersController {
         console.log('👋Hello from PlayersController', AppState.players);
         this.drawPlayers();
     }
+
+    // PlayersController.js
+    drawPlayers(){
+    const players = /* players array from AppState */
+    let playerContent = ''
+    players.forEach(player, /* ... */)
+    //... draw to page
+  }
 }
